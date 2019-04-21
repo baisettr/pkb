9a1f1d7f2a59182f19b9844e4936beeaa2f447fd
rk <ramakrishna8051@gmail.com>
2019-04-21 10:35:23 -0700

    client default files

---
+++ client/src/components/hook.js
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+
+const hook = (props) => {
+    return (
+        <div>
+            Hi There!
+    </div>
+    )
+}
+
+export default hook;
---
+++ client/src/containers/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+const Home = (props) => <div><h1>Parking Buddy</h1></div>;
+
+export default Home;
