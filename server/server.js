e2b0958ba52bd78da81a628f9eae0b5609151ac6
rk <ramakrishna8051@gmail.com>
2019-04-23 14:19:57 -0700

    server handling auth

--- server/server.js
+++ server/server.js
@@ -1,11 +1,42 @@
 const express = require('express');
+const models = require('./models');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportConfig = require('./services/auth');
+const MongoStore = require('connect-mongo')(session);
+const schema = require('./schema/schema');
 
 const app = express();
 
+const MONGO_URI = 'mongodb://graphql:graphql2019@ds251518.mlab.com:51518/graphql';
 
-app.use('/', (req, res) => {
-    res.send('There!');
-})
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI);
+mongoose.connection
+    .once('open', () => console.log('connected'))
+    .on('error', error => console.log('error', error));
+
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'xtcvhbjknlm',
+    store: new MongoStore({
+        url: MONGO_URI,
+        autoReconnect: true
+    })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/graphql', expressGraphQL({
+    schema,
+    graphiql: true
+}));
+
+module.exports = app;
 
 
 module.exports = app;
---
+++ server/services/auth.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = mongoose.model('user');
+
+passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+    User.findById(id, (err, user) => {
+        cb(err, user);
+    });
+});
+
+passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, cb) => {
+    User.findOne({ email: email.toLowerCase() }, (err, user) => {
+        if (err) return cb(err);
+        if (!user) return cb(null, false, 'Invalid Credentials');
+
+        user.comparePassword(password, (err, isMatch) => {
+            if (err) return cb(err);
+
+            if (isMatch) return cb(null, user);
+
+            return cb(null, false, 'Invalid Credentials');
+        });
+    });
+}));
+
+function signup({ email, password, name, req }) {
+    const user = new User({ email, password, name });
+    if (!email || !password) throw new Error('You must provide an email and password');
+
+    return User.findOne({ email })
+        .then(exUser => {
+            if (exUser) throw new Error('Email is in Use');
+            return user.save();
+        })
+        .then(user => {
+            return new Promise((resolve, reject) => {
+                req.login(user, (err) => {
+                    if (err) reject(err);
+                    resolve(user);
+                })
+            })
+        })
+
+};
+
+function login({ email, password, req }) {
+    return new Promise((resolve, reject) => {
+        passport.authenticate('local', (err, user) => {
+            if (!user) reject('Invalid Credentials');
+
+            req.login(user, () => resolve(user));
+        })({ body: { email, password } });
+    });
+};
+
+module.exports = { signup, login };
