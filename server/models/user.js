616f9891638c0eea6fbdf37a108635753074188f
rk <ramakrishna8051@gmail.com>
2019-04-23 14:20:11 -0700

    user mongo schema

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt-nodejs');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: { type: String },
+    password: { type: String },
+    name: { type: String }
+});
+
+UserSchema.pre('save', function save(next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, null, (err, hash) => {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        })
+    })
+});
+
+
+UserSchema.methods.comparePassword = function comparePassword(cpassword, cb) {
+    bcrypt.compare(cpassword, this.password, (err, isMatch) => {
+        cb(err, isMatch);
+    });
+};
+
+mongoose.model('user', UserSchema);
+
+
