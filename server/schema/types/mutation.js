29b18bf6f131c558e633ea15f7be33e37673bfd3
rk <ramakrishna8051@gmail.com>
2019-05-14 14:44:48 -0700

    updating mutations add parking, add booking

--- server/schema/types/mutation.js
+++ server/schema/types/mutation.js
@@ -1,12 +1,26 @@
 const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLFloat, GraphQLList, GraphQLInputObjectType } = graphql;
+const mongoose = require('mongoose');
+
+const Parking = mongoose.model('parking');
+const Booking = mongoose.model('booking');
+const User = mongoose.model('user');
+
 const AuthService = require('../../services/auth');
 
-const {
-    GraphQLObjectType,
-    GraphQLString
-} = graphql;
-
 const UserType = require('./user_type');
+const ParkingType = require('./parking_type');
+const BookingType = require('./booking_type');
+const { DateType } = require('./date_type');
+
+const SlotInput = new GraphQLInputObjectType({
+    name: 'SlotInput',
+    fields: () => ({
+        slotDate: { type: DateType },
+        price: { type: GraphQLFloat },
+        status: { type: GraphQLBoolean },
+    })
+});
 
 const mutation = new GraphQLObjectType({
     name: 'Mutation',
@@ -43,8 +57,59 @@ const mutation = new GraphQLObjectType({
                 return AuthService.login({ email, password, req });
             }
 
+        },
+        addBooking: {
+            type: BookingType,
+            args: {
+                bookingDate: { type: DateType },
+                parkingId: { type: GraphQLString }
+            },
+            async resolve(parentValue, { bookingDate, parkingId }, req) {
+                const userId = req.user._id;
+                const booking = await (new Booking({ bookingDate, parkingId, userId })).save()
+                const userUpdateBooking = await User.updateOne(
+                    { _id: userId },
+                    { $push: { bookings: booking._id } }
+                );
+                return booking
+            }
+        },
+        addParking: {
+            type: ParkingType,
+            args: {
+                street: { type: GraphQLString },
+                city: { type: GraphQLString },
+                state: { type: GraphQLString },
+                slots: {
+                    type: new GraphQLList(SlotInput)
+                }
+            },
+            async resolve(parentValue, { street, city, state, slots }, req) {
+                const userId = req.user._id;
+                const parking = await (new Parking({ street, city, state, slots, userId })).save()
+                const userUpdateParking = await User.updateOne(
+                    { _id: userId },
+                    { $push: { parkings: parking._id } }
+                );
+                return parking
+            }
         }
     }
 });
 
 module.exports = mutation;
+
+
+/*
+
+        addLyricToSong: {
+            type: SongType,
+            args: {
+                content: { type: GraphQLString },
+                songId: { type: GraphQLID }
+            },
+            resolve(parentValue, { content, songId }) {
+                return Song.addLyric(songId, content);
+            }
+        },
+*/
