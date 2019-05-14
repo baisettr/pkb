a3e22aa3e02cd5baf5666bc48f54e611058f4928
rk <ramakrishna8051@gmail.com>
2019-05-14 14:43:28 -0700

    updating parking schema

--- server/models/parking.js
+++ server/models/parking.js
@@ -4,21 +4,39 @@ const Schema = mongoose.Schema;
 const ParkingSchema = new Schema({
     street: { type: String },
     city: { type: String },
-    State: { type: String },
+    state: { type: String },
     zip: { type: Number },
     slots: [
-        { type: Date }
+        {
+            slotDate: { type: Date },
+            price: { type: Number },
+            status: Boolean
+        }
     ],
-    user: {
+    userId: {
         type: Schema.Types.ObjectId,
         ref: 'user'
     }
 })
 
-ParkingSchema.statics.addSlots = function (id, slot) {
+ParkingSchema.statics.addSlots = function (id, slots) {
     return this.findById(id)
         .then(parking => {
-            parking.slots.push(slot)
+            parking.slots = [...parking.slots, slots]
+            return parking.save();
+        })
+};
+
+ParkingSchema.statics.modifySlotStatus = function (id, slotUpdate) {
+    return this.findById(id)
+        .then(parking => {
+            const updatedSlots = parking.slots.map((slot) => {
+                if (slot.slotDate === slotUpdate.slotDate) {
+                    slot.status = slotUpdate.status;
+                }
+                return slot
+            })
+            parking.slots = updatedSlots;
             return parking.save();
         })
 };
