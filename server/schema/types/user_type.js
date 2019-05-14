f80826ddc8e79587be646fa3eefea6be1c9ea6e2
rk <ramakrishna8051@gmail.com>
2019-05-14 14:46:14 -0700

    updating user type with parkings and bookings

--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -1,13 +1,36 @@
+const mongoose = require('mongoose');
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLID, GraphQLString } = graphql;
+const ParkingType = require('./parking_type');
+const BookingType = require('./booking_type');
+const User = mongoose.model('user');
+
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLList } = graphql;
 
 const UserType = new GraphQLObjectType({
     name: 'UserType',
     fields: {
         id: { type: GraphQLID },
         email: { type: GraphQLString },
-        name: { type: GraphQLString }
+        name: { type: GraphQLString },
+        parkings: {
+            type: new GraphQLList(ParkingType),
+            resolve(parentValue) {
+                return User.findById(parentValue.id).populate('parkings')
+                    .then(user => {
+                        return user.parkings
+                    });
+            }
+        },
+        bookings: {
+            type: new GraphQLList(BookingType),
+            resolve(parentValue) {
+                return User.findById(parentValue.id).populate('bookings')
+                    .then(user => {
+                        return user.bookings
+                    });
+            }
+        }
     }
 });
 
