e122f0d8fa5ced2b606bd38f4a15f89390d5e185
rk <ramakrishna8051@gmail.com>
2019-05-14 14:45:37 -0700

    updating root queries, user parkings, user bookings, all parkings, parking, booking

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,18 +1,68 @@
+const mongoose = require('mongoose');
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLID } = graphql;
+const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLString, GraphQLNonNull } = graphql;
+
+const Booking = mongoose.model('booking');
+const Parking = mongoose.model('parking');
 
 const UserType = require('./user_type');
+const BookingType = require('./booking_type');
+const ParkingType = require('./parking_type');
 
 const RootQueryType = new GraphQLObjectType({
     name: 'RootQueryType',
-    fields: {
+    fields: () => ({
         user: {
             type: UserType,
             resolve(parentValue, args, req) {
                 return req.user
             }
+        },
+        userParkings: {
+            type: new GraphQLList(ParkingType),
+            resolve(rootValue, args, req) {
+                const userId = req.user._id;
+                return Parking.find({ userId });
+            }
+        },
+        userBookings: {
+            type: new GraphQLList(BookingType),
+            resolve(rootValue, args, req) {
+                const userId = req.user._id;
+                return Booking.find({ userId });
+            }
+        },
+        booking: {
+            type: BookingType,
+            args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+            resolve(parentValue, { id }) {
+                return Booking.findById(id);
+            }
+        },
+        parking: {
+            type: ParkingType,
+            args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+            resolve(parnetValue, { id }) {
+                return Parking.findById(id);
+            }
+        },
+        locationParkings: {
+            type: new GraphQLList(ParkingType),
+            args: {
+                city: { type: new GraphQLNonNull(GraphQLString) },
+                state: { type: new GraphQLNonNull(GraphQLString) },
+            },
+            resolve(rootValue, { city, state }) {
+                return Parking.find({ city, state });
+            }
+        },
+        allParkings: {
+            type: new GraphQLList(ParkingType),
+            resolve(rootValue, args, context) {
+                return Parking.find();
+            }
         }
-    }
+    })
 });
 
 module.exports = RootQueryType;
