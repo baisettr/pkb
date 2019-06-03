31a5db91938d6d34c0bfa0be5fec6b39a2f68664
rk <ramakrishna8051@gmail.com>
2019-06-03 15:16:53 -0700

    view the parkings of the user

--- client/src/containers/ViewParking.js
+++ client/src/containers/ViewParking.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import query from '../queries/UserParkings';
+import { graphql } from 'react-apollo';
+import { Button, Link } from '@material-ui/core';
+
 
 class ViewParking extends Component {
     constructor(props) {
         super(props);
     }
+    renderParkings() {
+        return this.props.data.userParkings.map(({ id, street, slotNo, slots }) => {
+            const [slot] = slots;
+            return (
+                <li key={id} >
+                    <h4>{street}</h4>
+                    <p>Date - {slot.slotDate}, Price: ${slot.price}, Status: {slot.status ? "Available" : "Booked"}</p>
+                </li>
+            );
+        });
+    }
     render() {
+        if (this.props.data.loading) { return <div>Loading...</div>; }
+
         return (
-            <div>
-                Viw the details of the parking chosen
+            <div style={{ margin: "100px", alignContent: "center" }}>
+                <h4>List of User Parkings</h4>
+                <ul >
+                    {this.renderParkings()}
+                </ul>
             </div>
-        )
+        );
     }
 }
 
-export default ViewParking;
+export default graphql(query)(ViewParking);
