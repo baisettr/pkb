b0a7aa81a7d69cb883d8759c1e8a47f985638d40
rk <ramakrishna8051@gmail.com>
2019-06-11 16:55:15 -0700

    Home ref

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default () => {
-    return <div><h1>Parking Buddy</h1></div>
+    return <div style={{ margin: "100px", alignContent: "center", textAlign: "center" }}>
+        <h1>Parking Space</h1>
+    </div>
 };
