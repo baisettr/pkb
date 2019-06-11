cf475281001900b36bd2999dda2c7e925ef55fa3
rk <ramakrishna8051@gmail.com>
2019-06-11 16:52:53 -0700

    updating routes

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -6,15 +6,13 @@ import { ApolloProvider } from 'react-apollo';
 
 import '../styles/App.css';
 
-import Login from './Login';
-import Signin from './signinside';
-import Signup from './Signup';
+import SignIn from './SignIn';
 import Home from './Home';
 import Dashboard from './Dashboard';
 import requireAuth from './requireAuth';
 
-import Mat from './mat';
 import AppBar from './AppBar';
+import NavBar from './NavBar';
 import AddParking from './AddParking';
 import SearchParkings from './SearchParkings';
 import ViewParking from './ViewParking';
@@ -43,16 +41,14 @@ class App extends Component {
         <AppBar />
         <Switch>
           <Route path="/" exact component={Dashboard} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/dashboard" component={requireAuth(Home)} />
+          <Route path="/login" component={SignIn} />
+          <Route path="/home" component={requireAuth(Home)} />
 
           <Route path="/add" component={requireAuth(AddParking)} />
-          <Route path="/search" component={requireAuth(SearchParkings)} />
+          <Route path="/search" component={SearchParkings} />
           <Route path="/view" component={requireAuth(ViewParking)} />
           <Route path="/bookings" component={requireAuth(ViewBookings)} />
           <Route path="/book/:bookId/:bookDate/:bookPrice/:bookAddr" component={requireAuth(BookParking)} />
-          <Route path="/mat" component={Mat} />
         </Switch >
       </ApolloProvider>
     )
