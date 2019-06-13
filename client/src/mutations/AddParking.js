236465f3850dd5a85aa71dcaafcd244bb980b470
rk <ramakrishna8051@gmail.com>
2019-06-12 17:02:20 -0700

    update client mutations

--- client/src/mutations/AddParking.js
+++ client/src/mutations/AddParking.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export default gql`
-mutation AddParking($slotNo: String, $street: String, $city: String, $state: String, $zip: Int, $slots: [SlotInput]) {
-  addParking(slotNo: $slotNo, street: $street, city: $city, state: $state, zip: $zip, slots: $slots) {
+mutation AddParking($slotNo: String, $street: String, $city: String, $state: String, $zip: Int, $slots: [SlotInput],$location:LocationInput) {
+  addParking(slotNo: $slotNo, street: $street, city: $city, state: $state, zip: $zip, slots: $slots,location: $location) {
     id
   }
 }
--- client/src/mutations/Signup.js
+++ client/src/mutations/Signup.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export default gql`
-mutation signup($email: String, $password: String) {
-    signup(email: $email, password: $password) {
+mutation signup($email: String, $password: String, $firstName: String, $lastName: String) {
+    signup(email: $email, password: $password, firstName: $firstName, lastName: $lastName) {
       id
       email
     }
