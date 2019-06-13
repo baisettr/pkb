f7795528f26caba3ae9625411ece0d3995f5cd6f
rk <ramakrishna8051@gmail.com>
2019-06-12 17:01:32 -0700

    New layout for signin and sign up

--- client/src/containers/SignIn.js
+++ client/src/containers/SignIn.js
@@ -8,10 +8,11 @@ import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+
+import { Person,LockOutlinedIcon } from '@material-ui/icons';
 
 import query from '../queries/User';
 import mutationLogin from '../mutations/Login';
@@ -82,7 +83,7 @@ function Auth(props) {
     const SignIn = () => (
         <div className={classes.paper}>
             <Avatar className={classes.avatar}>
-                <LockOutlinedIcon />
+                <Person />
             </Avatar>
             <Typography component="h1" variant="h5">
                 Sign in
@@ -143,7 +144,7 @@ function Auth(props) {
     const SignUp = () => (
         <div className={classes.paper}>
             <Avatar className={classes.avatar}>
-                <LockOutlinedIcon />
+                <Person />
             </Avatar>
             <Typography component="h1" variant="h5">
                 Sign up
